44a17c788e3ba633d5635ba84bb53aa01ba2e6bb
mably <f.mably@gmail.com>
2014-09-11 15:16:25 +0200

    Meta moved to btcwire for use in btcnet

--- ppc.go
+++ ppc.go
@@ -1,91 +1,21 @@
+// Copyright (c) 2014-2014 PPCD developers.
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
 package btcutil
 
 import (
 	"bytes"
-	"encoding/binary"
 	"github.com/conformal/btclog"
 	"github.com/mably/btcwire"
-	"io"
-	"math/big"
 	"time"
 )
 
-type Meta struct {
-	StakeModifier         uint64
-	StakeModifierChecksum uint32 // checksum of index; in-memeory only (main.h)
-	HashProofOfStake      btcwire.ShaHash
-	Flags                 uint32
-	ChainTrust            big.Int
-}
-
-func (m *Meta) Serialize(w io.Writer) error {
-	e := binary.Write(w, binary.LittleEndian, &m.StakeModifier)
-	if e != nil {
-		return e
-	}
-	binary.Write(w, binary.LittleEndian, &m.StakeModifierChecksum)
-	if e != nil {
-		return e
-	}
-	binary.Write(w, binary.LittleEndian, &m.Flags)
-	if e != nil {
-		return e
-	}
-	binary.Write(w, binary.LittleEndian, &m.HashProofOfStake)
-	if e != nil {
-		return e
-	}
-	bytes := m.ChainTrust.Bytes()
-	var blen byte
-	blen = byte(len(bytes))
-	binary.Write(w, binary.LittleEndian, &blen)
-	if e != nil {
-		return e
-	}
-	binary.Write(w, binary.LittleEndian, &bytes)
-	if e != nil {
-		return e
-	}
-	return nil
-}
-
-func (m *Meta) Deserialize(r io.Reader) error {
-	e := binary.Read(r, binary.LittleEndian, &m.StakeModifier)
-	if e != nil {
-		return e
-	}
-	e = binary.Read(r, binary.LittleEndian, &m.StakeModifierChecksum)
-	if e != nil {
-		return e
-	}
-	e = binary.Read(r, binary.LittleEndian, &m.Flags)
-	if e != nil {
-		return e
-	}
-	e = binary.Read(r, binary.LittleEndian, &m.HashProofOfStake)
-	if e != nil {
-		return e
-	}
-
-	var blen byte
-	e = binary.Read(r, binary.LittleEndian, &blen)
-	if e != nil {
-		return e
-	}
-	var arr = make([]byte, blen)
-	e = binary.Read(r, binary.LittleEndian, &arr)
-	if e != nil {
-		return e
-	}
-	m.ChainTrust.SetBytes(arr)
-	return nil
-}
-
-func (b *Block) Meta() *Meta {
+func (b *Block) Meta() *btcwire.Meta {
 	if b.meta != nil {
 		return b.meta
 	}
-	b.meta = new(Meta)
+	b.meta = new(btcwire.Meta)
 	return b.meta
 }
 
@@ -134,7 +64,7 @@ func (b *Block) BytesWithMeta() ([]byte, error) {
 
 // NewBlock returns a new instance of a bitcoin block given an underlying
 // btcwire.MsgBlock.  See Block.
-func NewBlockWithMetas(msgBlock *btcwire.MsgBlock, meta *Meta) *Block {
+func NewBlockWithMetas(msgBlock *btcwire.MsgBlock, meta *btcwire.Meta) *Block {
 	return &Block{
 		msgBlock:    msgBlock,
 		blockHeight: BlockHeightUnknown,
